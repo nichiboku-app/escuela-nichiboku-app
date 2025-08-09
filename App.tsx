8c35fc97f5ca2dbc4a6e15d224c99395e7bd1de5
unknown <bunkan.nichiboku@gmail.com>
2025-08-09 01:01:33 -0600

    Actualización de HomeScreen y nuevos assets

--- App.tsx
+++ App.tsx
@@ -3,12 +3,13 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
 
-import HomeScreen from './app/(tabs)';
 import BienvenidaScreen from './src/screens/BienvenidaScreen';
 import CrearCuentaScreen from './src/screens/CrearCuentaScreen';
+import HomeScreen from './src/screens/HomeScreen';
 import LoginScreen from './src/screens/LoginScreen';
 import OnboardingScreen from './src/screens/OnboardingScreen';
 import SplashScreen from './src/screens/SplashScreen';
+
 import { RootStackParamList } from './types';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
@@ -23,7 +24,6 @@ export default function App() {
         <Stack.Screen name="CrearCuenta" component={CrearCuentaScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Bienvenida" component={BienvenidaScreen} />
-
 
       </Stack.Navigator>
     </NavigationContainer>
